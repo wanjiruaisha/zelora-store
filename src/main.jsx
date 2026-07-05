1928a4ca6d63641b989eef7d8e802eb640f9d7f1
wanjiruaisha <wanjiruaisha30@gmail.com>
2026-07-05 11:21:38 +0300

    feat: wrap app with context providers

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
+import { ProductProvider } from './context/ProductContext'
+import { FilterProvider } from './context/FilterContext'
+import { CartProvider } from './context/CartContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-          <App />
+      <ProductProvider>
+        <FilterProvider>
+          <CartProvider>
+              <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductProvider>
     </AuthProvider>
   </StrictMode>,
 )
